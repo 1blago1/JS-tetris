c57a62d678a6a4545326a2e6794f285fdf83c872
1blago1e <nikitasimple53@gmail.comm>
2023-01-03 18:41:20 +0300

    add move figure

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ import Game from "./src/game.js";
 
 const game = new Game();
 
+window.game = game; //добавляем объект game глобальный объект window
+
 console.log(game);
--- src/game.js
+++ src/game.js
@@ -30,8 +30,60 @@ export default class Game {
     y: 0,
     blocks: [
       [0, 1, 0],
-      [0, 1, 1],
-      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0],
     ],
   };
+
+  movePieceLeft() {
+    this.activePiece.x -= 1;
+
+    if (this.hasCollisions()) {
+      this.activePiece.x += 1;
+    }
+  }
+  movePieceRight() {
+    this.activePiece.x += 1;
+    if (this.hasCollisions()) {
+      this.activePiece.x -= 1;
+    }
+  }
+  movePieceDown() {
+    this.activePiece.y += 1;
+    if (this.hasCollisions()) {
+      this.activePiece.y -= 1;
+      this.lockPiece();
+    }
+  }
+
+  hasCollisions() {
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (
+          blocks[y][x] &&
+          (this.playfield[pieceY + y] === undefined ||
+            this.playfield[pieceY + y][pieceX + x] === undefined ||
+            this.playfield[pieceY + y][pieceX + x])
+        ) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  lockPiece() {
+    const { y: pieceY, x: pieceX, blocks } = this.activePiece;
+
+    for (let y = 0; y < blocks.length; y++) {
+      for (let x = 0; x < blocks[y].length; x++) {
+        if (blocks[y][x]) {
+          this.playfield[pieceY + y][pieceX + x] = blocks[y][x];
+        }
+      }
+    }
+  }
 }
