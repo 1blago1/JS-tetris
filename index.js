5bf6436faf23e900aa392e694a1748d0a992e3e9
1blago1e <nikitasimple53@gmail.comm>
2023-01-08 00:41:30 +0300

    add class VIEW

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 import Game from "./src/game.js";
+import View from "./src/view.js";
+
+const root = document.querySelector("#root");
 
 const game = new Game();
+const view = new View(root, 320, 640, 20, 10);
 
 window.game = game; //добавляем объект game глобальный объект window
+window.view = view;
 
-console.log(game);
+document.addEventListener("keydown", (event) => {
+  switch (event.keyCode) {
+    case 65: //LEFT ARROW
+      game.movePieceLeft();
+      view.render(game.getState());
+      break;
+    case 87: //UP ARROW
+      game.rotatePiece();
+      view.render(game.getState());
+      break;
+    case 68: //RIGHT ARROW
+      game.movePieceRight();
+      view.render(game.getState());
+      break;
+    case 83: // DOWN ARROW
+      game.movePieceDown();
+      view.render(game.getState());
+      break;
+  }
+});
